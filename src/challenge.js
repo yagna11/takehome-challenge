f521f03271e48560f9389e8f07eccff636f3a5ee
Yagna Patel <yagnapatel@yagnas-mbp.home>
2023-09-07 18:08:43 -0400

    updated-challenge.js-formatting

--- src/challenge.js
+++ src/challenge.js
@@ -2,58 +2,63 @@ const fs = require("fs");
 
 // Load and parse the JSON files with error handling
 function loadAndParseJsonFile(filename) {
-    try {
-        return JSON.parse(fs.readFileSync(`${__dirname}/data/${filename}`, "utf-8"));
-    } catch (error) {
-        console.error(`Error reading or ${__dirname}/data/${filename}:`, error);
-        process.exit(1);
-    }
+  try {
+    return JSON.parse(
+      fs.readFileSync(`${__dirname}/data/${filename}`, "utf-8")
+    );
+  } catch (error) {
+    console.error(`Error reading or ${__dirname}/data/${filename}:`, error);
+    process.exit(1);
+  }
 }
 
 // Format individual user's data
 function formatUserData(user, company) {
-    const previousTokenBalance = user.tokens;
-    user.tokens += company.top_up;
+  const previousTokenBalance = user.tokens;
+  user.tokens += company.top_up;
 
-    const updatedUserInfo = `
+  const updatedUserInfo = `
             ${user.last_name}, ${user.first_name}, ${user.email}
               Previous Token Balance, ${previousTokenBalance}
               New Token Balance ${user.tokens}`;
-              
-    return updatedUserInfo;
+
+  return updatedUserInfo;
 }
 
 // Main logic to generate the output string
 function generateFormattedOutput(companies, users) {
-    let output = "";
+  let output = "";
 
-    for (const company of companies) {
-        let totalCompanyTopup = 0;
-        let emailedUsers = [];
-        let notEmailedUsers = [];
+  for (const company of companies) {
+    let totalCompanyTopup = 0;
+    let emailedUsers = [];
+    let notEmailedUsers = [];
 
-        const activeCompanyUsers = users.filter(u => u.company_id === company.id && u.active_status);
-        
-        for (const user of activeCompanyUsers) {
-            const userInfo = formatUserData(user, company);
-            totalCompanyTopup += company.top_up;
+    const activeCompanyUsers = users.filter(
+      (u) => u.company_id === company.id && u.active_status
+    );
 
-            if (user.email_status && company.email_status) {
-                emailedUsers.push(userInfo);
-            } else {
-                notEmailedUsers.push(userInfo);
-            }
-        }
+    for (const user of activeCompanyUsers) {
+      const userInfo = formatUserData(user, company);
+      totalCompanyTopup += company.top_up;
 
-        output += `
-    Company Id: ${company.id}
-    Company Name: ${company.name}
-    Users Emailed:${emailedUsers.join('')}
-    Users Not Emailed:${notEmailedUsers.join('')}
-        Total amount of top ups for ${company.name}: ${totalCompanyTopup}`;
+      if (user.email_status && company.email_status) {
+        emailedUsers.push(userInfo);
+      } else {
+        notEmailedUsers.push(userInfo);
+      }
     }
 
-    return output;
+    output += `
+    Company Id: ${company.id}
+    Company Name: ${company.name}
+    Users Emailed:${emailedUsers.join("")}
+    Users Not Emailed:${notEmailedUsers.join("")}
+            Total amount of top ups for ${company.name}: ${totalCompanyTopup}
+            `;
+  }
+
+  return output;
 }
 
 const companies = loadAndParseJsonFile("companies.json");
@@ -63,4 +68,4 @@ companies.sort((a, b) => a.id - b.id);
 users.sort((a, b) => a.last_name.localeCompare(b.last_name));
 
 const output = generateFormattedOutput(companies, users);
-fs.writeFileSync(`${__dirname}/output//output.txt`, output);
+fs.writeFileSync(`${__dirname}/output/output.txt`, output);
