9ef88e2f0ce25127e4478fe7ca883937757b9015
Yagna Patel <yagnapatel@yagnas-mbp.home>
2023-09-07 17:28:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+Code Challenge Solution
+
+This project is a solution to a code challenge outlined in challenge.txt.
+
+Overview
+You are provided with two JSON files: one representing users and another representing companies. The code processes these files and produces a formatted output.txt file as per the defined criteria.
+
+Challenge Instructions:
+For detailed challenge instructions, please refer to the challenge.txt file in the root directory.
+
+File structure: 
+TAKEHOME/
+│
+├── src/
+│   ├── data/
+│   │   ├── companies.json      # Contains company data
+│   │   └── users.json          # Contains user data
+│   │
+│   ├── output/
+│   │   ├── example_output.txt  # Provided example of expected output
+│   │   └── output.txt          # Generated output from the challenge.js script
+│   │
+│   └── challenge.js            # The main script to process data and generate the output.txt
+│
+├── challenge.txt               # Contains the challenge instructions
+└── README.md                   # This documentation
+
+Instructions to run it on your system - 
+1. Clone this repo
+2. Ensure you have Node.js installed.
+3. Navigate to the root directory (takehome)
+4. Run the script using the command:
+        node src/challenge.js
+5. Once executed, the output.txt inside the output folder will be updated with the processed data
+
+Implementation:
+The challenge.js script processes the company and user data stored in the data folder. Based on defined criteria, it calculates the token top-ups for active users and generates an output indicating which users were potentially emailed about the top-up. The final result is written to the output.txt file in the output directory.
+
---
+++ src/challenge.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+
+// Load and parse the JSON files with error handling
+function loadAndParseJsonFile(filename) {
+    try {
+        return JSON.parse(fs.readFileSync(`${__dirname}/data/${filename}`, "utf-8"));
+    } catch (error) {
+        console.error(`Error reading or ${__dirname}/data/${filename}:`, error);
+        process.exit(1);
+    }
+}
+
+// Format individual user's data
+function formatUserData(user, company) {
+    const previousTokenBalance = user.tokens;
+    user.tokens += company.top_up;
+
+    const updatedUserInfo = `
+            ${user.last_name}, ${user.first_name}, ${user.email}
+              Previous Token Balance, ${previousTokenBalance}
+              New Token Balance ${user.tokens}`;
+              
+    return updatedUserInfo;
+}
+
+// Main logic to generate the output string
+function generateFormattedOutput(companies, users) {
+    let output = "";
+
+    for (const company of companies) {
+        let totalCompanyTopup = 0;
+        let emailedUsers = [];
+        let notEmailedUsers = [];
+
+        const activeCompanyUsers = users.filter(u => u.company_id === company.id && u.active_status);
+        
+        for (const user of activeCompanyUsers) {
+            const userInfo = formatUserData(user, company);
+            totalCompanyTopup += company.top_up;
+
+            if (user.email_status && company.email_status) {
+                emailedUsers.push(userInfo);
+            } else {
+                notEmailedUsers.push(userInfo);
+            }
+        }
+
+        output += `
+    Company Id: ${company.id}
+    Company Name: ${company.name}
+    Users Emailed:${emailedUsers.join('')}
+    Users Not Emailed:${notEmailedUsers.join('')}
+        Total amount of top ups for ${company.name}: ${totalCompanyTopup}`;
+    }
+
+    return output;
+}
+
+const companies = loadAndParseJsonFile("companies.json");
+const users = loadAndParseJsonFile("users.json");
+
+companies.sort((a, b) => a.id - b.id);
+users.sort((a, b) => a.last_name.localeCompare(b.last_name));
+
+const output = generateFormattedOutput(companies, users);
+fs.writeFileSync(`${__dirname}/output//output.txt`, output);
